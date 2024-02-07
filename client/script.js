3a9b60633000315b7e7017822883d8173b6548a9
moon-strider <iamthekidyoudoknowwhatimean@gmail.com>
2024-02-07 22:49:58 +0300

    initial commit

---
+++ client/script.js
@@ -0,0 +1,23 @@
+async function ping() {
+    let data;
+
+    fetch('http://localhost:8000/ping')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(json => {
+        data = json;
+        let result_container = document.getElementsByClassName("result");
+        if (result_container.length > 0) {
+            result_container[0].innerText = data["ping"];
+        } else {
+            console.error("Failed to retreive result container");
+        }
+    })
+    .catch(error => {
+        console.error('There was an error with the fetch operation:', error);
+    });
+}
